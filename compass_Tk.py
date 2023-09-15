b7284084a1cb38e832d61b694352c4a4f8ff90b7
Sumedh Sopan Nagrale <51673422+SumedhNagrale@users.noreply.github.com>
2023-09-15 13:23:42 -0500

    Optmized Code
    Removing the loops and replacing them with the numpy where function

--- compass_Tk.py
+++ compass_Tk.py
@@ -13,38 +13,48 @@ Following modifications done:
 6. assigned correct initial values
 7. renamed the variables
 8. Changed the comment from single to double quotes
+
+15th Sep 2023
+Following modifications were made to
+1. Removed the loops which were replaced by np.where
+2. Optimized the code for speed.
+3. Added variables separately
 """
 import numpy as np
 
 
 def compass_Tk(tIn, tParam):
     """
+    Build the T matrix based on the input data and parameters.
+
+    Args:
+    tIn (numpy.ndarray): Input data.
+    tParam (dict): Parameters including 'nc', 'xM', 'cLinkMap', 'cConstantUpdate'.
     tParam.nd = number of continuous
     tParam.nx = number of hidden state
     tParam.dLinkMap, the link function between X and continuous variable
+
+    Returns:
+    list of numpy.ndarray: List of T matrices.
+    numpy.ndarray: P matrix.
     """
-    t_matrix = []  # This is T, an array of arrays
-    for k in range(0, len(tIn)):
-        temp = np.ones((tParam['nc'], len(tParam['xM'])))
-        for i in range(0, tParam['nc']):
-            for j in range(0, len(tParam['xM'])):
-                if tParam['cLinkMap'][i, j]:
-                    temp[i, j] = tIn[k, tParam['cLinkMap'][i, j]]
-        t_matrix.append(temp)
+    # Variables
+    cLinkMap = tParam['cLinkMap']
+    cConstantUpdate = tParam['cConstantUpdate']
 
-    p_matrix = np.zeros((tParam['nc'], tParam['nIn']))
-    for i in range(0, tParam['nc']):
-        for j in range(0, tParam['nIn']):
-            if tParam['cConstantUpdate'][i, j]:
-                p_matrix[i, j] = 1
+    # Create a list of T matrices using a list comprehension
+    t_matrix = [np.where(cLinkMap, tIn[k, cLinkMap], 1.0) for k in range(len(tIn))]
 
+    # Create the P matrix using NumPy operations
+    p_matrix = np.where(cConstantUpdate, 1.0, 0.0)
     return t_matrix, p_matrix
 
-"""
+
+'''
 # Test case: Single state
 Param = {'nc': 3, 'nIn': 3, 'xM': np.array([1, 6, 8]), 'cLinkMap': np.array([[0, 1, 0], [1, 1, 0], [1, 1, 1]]),
          'cConstantUpdate': np.array([[0, 1, 0], [1, 1, 0], [1, 1, 1]])}
 [T, P] = compass_Tk(tIn=np.array([[10253, 65, 3, 9, 67, 98], [1234, 5, 3, 9, 67, 98]]), tParam=Param)
 print(T)
 print(P)
-"""
+'''
