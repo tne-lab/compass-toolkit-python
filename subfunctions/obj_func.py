03d11ff10928ed5bc59b562a34eb116a4a0699d2
Sumedh Sopan Nagrale <51673422+SumedhNagrale@users.noreply.github.com>
2023-09-20 15:25:54 -0500

    main function
    this is done in order to add all the imports at once.

---
+++ subfunctions/obj_func.py
@@ -0,0 +1,9 @@
+from subfunctions import bernoulli_param
+from subfunctions import gamma_param_cd
+from subfunctions import gamma_param_full
+from subfunctions import gamma_param_minus_S
+from subfunctions import gamma_param_minus_v
+from subfunctions import normal_param_cd
+from subfunctions import normal_param_v
+from subfunctions import normal_param_cdv
+
